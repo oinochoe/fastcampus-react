709db5e64e384b333e80da1e3caf7dea0fc52174
yeongminkim <oinochoe@naver.com>
2020-09-17 02:12:09 +0900

    클래스형 컴포넌트 연습¸°

--- src/Hello.js
+++ src/Hello.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Hello({ color, name, isSpecial }) {
+class Hello extends Component {
+    render() {
+        const { color, isSpecial, name } = this.props;
+        return (
+            <div style={{ color }}>
+                {isSpecial && <b>*</b>}
+                안녕하세요 {name}
+            </div>
+        );
+    }
+}
+
+/* function Hello({ color, name, isSpecial }) {
     return (
         <div style={{ color }}>
             {isSpecial && <b>*</b>}
@@ -8,7 +20,7 @@ function Hello({ color, name, isSpecial }) {
         </div>
     );
 }
-
+ */
 Hello.defaultProps = {
     name: '영민',
 };
