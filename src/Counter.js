e9e194dfa56640f19efab74a7f24dda0c1949984
yeongminkim <oinochoe@naver.com>
2020-09-19 16:01:01 +0900

    setState 실습

--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    state = {
+        counter: 0,
+        fixed: 0,
+        updateMe: {
+            toggleMe: false,
+            dontChangeMe: 1,
+        },
+    };
+
+    handleIncrease = () => {
+        // this.setState({
+        //     counter: this.state.counter + 1,
+        // });
+        this.setState((state) => ({
+            counter: state.counter + 1,
+        }));
+    };
+
+    handleDecrease = () => {
+        this.setState({
+            counter: this.state.counter - 1,
+        });
+    };
+
+    handleToggle = () => {
+        this.setState({
+            updateMe: {
+                ...this.state.updateMe,
+                toggleMe: !this.state.updateMe.toggleMe,
+            },
+        });
+    };
+
     render() {
         return (
             <div>
-                <h1>0</h1>
-                <button>+1</button>
-                <button>-1</button>
+                <h1>{this.state.counter}</h1>
+                <button onClick={this.handleIncrease}>+1</button>
+                <button onClick={this.handleDecrease}>-1</button>
+                <p>픽스된 값 : {this.state.fixed}</p>
             </div>
         );
     }
