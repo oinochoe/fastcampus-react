205a5a412711b90367b63d437f913a440cfe8488
yeongminkim <oinochoe@naver.com>
2020-09-19 17:37:07 +0900

    setnry 에러 트래킹 추가

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 
 // import App from './App';
 import Counter from './Counter';
+
+Sentry.init({
+    dsn: 'https://87f85b6de84046158b59bd1b264f9314@o450153.ingest.sentry.io/5434333',
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+});
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
