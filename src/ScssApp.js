794a4a8bedf4854aea9d9abf3c8a6de9ce60974a
yeongminkim <oinochoe@naver.com>
2020-09-23 02:01:17 +0900

    margin-left issue 수정

--- src/ScssApp.js
+++ src/ScssApp.js
@@ -6,6 +6,8 @@ function ScssApp() {
     return (
         <div className="App">
             <div className="buttons">
+                <Button size="large">Button</Button>
+                <Button size="small">Button</Button>
                 <Button>Button</Button>
             </div>
         </div>
